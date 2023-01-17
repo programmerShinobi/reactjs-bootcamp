1e0e838f9fe4b6e3c50a59a13dab629550deb208
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-17 22:42:28 +0700

    First commit reactjs

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+import MyComponent from './latihan-component/bootstrap-test';
+import CalculatorSubtraction from './latihan-component/calculator-subtraction';
+import CalculatorMultiplication from './latihan-component/calculator-multiplication';
+import CalculatorAddition from './latihan-component/calculator-addition';
+import CalculatorDivision from './latihan-component/calculator-division';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <MyComponent /> */}
+    <CalculatorAddition />
+    <CalculatorSubtraction />
+    <CalculatorMultiplication />
+    <CalculatorDivision />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
